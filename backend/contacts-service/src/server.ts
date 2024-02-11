1bb8d0b938a7133fba64e34ea8a53c766dbe2dc3
Alisson Cavalcanti Galvão <amcom-alisson.cavalcanti@ailos.coop.br>
2024-02-11 01:24:24 -0300

    update microservices to use db.ts in commons

---
+++ backend/contacts-service/src/server.ts
@@ -0,0 +1,21 @@
+import app from "./app";
+import database from 'ms-commons/data/db';
+
+(async() => {
+    try{
+
+        //obtendo as variáveis de ambiente
+        const {PORT, DB_NAME} = process.env;
+
+        //iniciando conexão ao database com sincronismo dos modelos
+        await database.sync();
+        console.log(`## INFO ## - Running Database ${DB_NAME}`);
+
+        // Startando o server
+        await app.listen(PORT);
+        console.log(`## INFO ## - Start Server in port ${PORT}`);
+
+    }catch(e){
+        console.log(e);
+    }
+})();
