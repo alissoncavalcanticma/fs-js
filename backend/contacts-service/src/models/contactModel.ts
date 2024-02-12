8892da229b4a8d54e609e487d09df901dfd015ae
Alisson Cavalcanti Galvão <amcom-alisson.cavalcanti@ailos.coop.br>
2024-02-12 04:41:41 -0300

    create contactModel.ts

--- backend/contacts-service/src/models/contactModel.ts
+++ backend/contacts-service/src/models/contactModel.ts
@@ -2,10 +2,40 @@ import Sequelize, {Optional, Model} from 'sequelize';
 import database from 'ms-commons/data/db';
 import {IContact} from './contact';
 
+//interface para definir atributo optional para casos de Update
 interface IContactCreationAttributes extends Optional<IContact, "id">{}
 
+//export de interface que cruza os types definidos em IContact com a interface Optional
 export interface IContactModel extends Model<IContact, IContactCreationAttributes>, IContact {}
 
-/*export default database.define<IContactModel>('contact', {
-   
-})*/
+export default database.define<IContactModel>('contac', {
+    id:{
+        type: Sequelize.INTEGER.UNSIGNED,
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true
+    },
+    accountId:{
+        type: Sequelize.INTEGER.UNSIGNED,
+        allowNull: false
+    },
+    name:{
+        type: Sequelize.STRING(150),
+        allowNull: true
+    },
+    email:{
+        type: Sequelize.STRING(150),
+        allowNull: false,
+        unique: true
+    },
+    phone:{
+        type: Sequelize.STRING(11),
+        allowNull: true
+    },
+    status:{
+        type: Sequelize.SMALLINT.UNSIGNED,
+        allowNull: false,
+        defaultValue: 100
+    }
+
+})
