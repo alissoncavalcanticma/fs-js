aa4bb382a99cf0ff3e83b402be8681ab6b94a15e
Alisson Cavalcanti Galvão <amcom-alisson.cavalcanti@ailos.coop.br>
2024-02-13 04:36:51 -0300

    Adjust do run microservices. Save point

--- backend/accounts-service/src/routes/middlewares.ts
+++ backend/accounts-service/src/routes/middlewares.ts
@@ -1,24 +1,24 @@
 import {Request, Response} from 'express';
 
 //import commonsMiddleware from 'ms-commons/api/routes/middlewares';
-import middlewareCommons from 'ms-commons/api/routes/middlewares';
+import commonsMiddleware from 'ms-commons/api/routes/middlewares';
 import {accountSchema, loginSchema, accountUpdateSchema} from '../models/accountSchemas';
 
 
 function validateAccountSchema(req: Request, res: Response, next: any){
-    return middlewareCommons.validateSchema(accountSchema as any, req as any, res as any, next);
+    return commonsMiddleware.validateSchema(accountSchema as any, req as any, res as any, next);
 }
 
 function validateUpdateAccountSchema(req: Request, res: Response, next: any){
-    return middlewareCommons.validateSchema(accountUpdateSchema as any, req as any, res as any, next);
+    return commonsMiddleware.validateSchema(accountUpdateSchema as any, req as any, res as any, next);
 }
 
 function validateLoginSchema(req: Request, res: Response, next: any){
-    return middlewareCommons.validateSchema(loginSchema as any, req as any, res as any, next);
+    return commonsMiddleware.validateSchema(loginSchema as any, req as any, res as any, next);
 }
 
 async function validateAuth(req: Request, res: Response, next: any){
-    return middlewareCommons.validateAuth(req as any, res as any, next);
+    return commonsMiddleware.validateAuth(req as any, res as any, next);
 }
 
 export {validateAccountSchema, validateLoginSchema, validateUpdateAccountSchema, validateAuth}
--- backend/contacts-service/src/controllers/contacts.ts
+++ backend/contacts-service/src/controllers/contacts.ts
@@ -1,7 +1,12 @@
 import {Request, Response} from 'express';
+import commonsController from 'ms-commons/controllers/controller';
+import { Token } from 'ms-commons/api/auth';
+import repository from 'src/models/contactRepository';
 
 async function getContacts(req: Request, res: Response, next: any){
-    res.json([]);
+    const token = commonsController.getToken(res) as Token;
+    const contacts = await repository.findAll(token.accountId);
+    res.json(contacts);
 }
 
 export default {getContacts}
