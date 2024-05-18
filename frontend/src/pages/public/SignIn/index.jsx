baf0c7d52eae7008869ff6287816ec8a143dc6e6
Alisson C <alissoncavalcanticma@gmail.com>
2024-05-18 03:10:37 -0300

    Create form Signin

--- frontend/src/pages/public/SignIn/index.jsx
+++ frontend/src/pages/public/SignIn/index.jsx
@@ -1,31 +1,44 @@
 
 import React from 'react';
 
-import {Button, Form, Container, Row, Col} from 'react-bootstrap';
+import {Button, Form, Container, Row, Col, FormGroup} from 'react-bootstrap';
 import Logo from '../../../assets/logo.png';
-
-handleSigin = async(event) => {
-    //Evita que seja executada a request para própria Pagina
-    event.preventDefault();    
-
-    //Aula: 00:30 seg 
-}
 
 
 //Function SignIn
 class Signin extends React.Component{
+
+    handleSigin = async(event) => {
+        //Evita que seja executada a request para própria Pagina
+        event.preventDefault();    
+    }
+
     render(){
         return(
             <Container>
-                <Row>
-                    <Col>
+                <Row className="justify-align-content-md-center"> 
+                    <Col xs={12} md={5}>
                         <div>
                             <img src={Logo} alt='MailShrimp'/>
                         </div>
                         <h2>Login</h2>
                         <p>Informe seus dados para autenticar: </p>
-                        <Form onSubmit={handleSigin}>
-
+                        <Form onSubmit={this.handleSigin}>
+                            <Form.Group controlId='emailGroup'>
+                                <Form.Label>E-mail:</Form.Label>
+                                <Form.Control
+                                    type="email"
+                                    placeholder="Digite seu e-mail"/>
+                            </Form.Group>
+                            <Form.Group controlId="passwordGroup">
+                                <Form.Label>Senha:</Form.Label>
+                                <Form.Control
+                                    type="password"
+                                    placeholder="Digite sua senha" />
+                            </Form.Group>
+                            <Button block variant="secondary" type="submit">
+                                Fazer Login
+                            </Button>
                         </Form>
                     </Col>
                 </Row>
