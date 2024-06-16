8a2500ece3c53d4a7a51fc97a78b7edcd3b34b90
Alisson Cavalcanti Galvão <alissoncavalcanticma@gmail.com>
2024-06-16 02:20:46 -0300

    import cors dependency for external communication

--- backend/__commons__/src/api/app.ts
+++ backend/__commons__/src/api/app.ts
@@ -2,12 +2,15 @@
 import express, {Router} from 'express';
 import bodyParser from 'body-parser';
 import helmet from 'helmet';
+import cors from 'cors';
 
 export default (router: Router) => {
     const app = express();
     app.use(helmet());
+    app.use(cors());
     app.use(bodyParser.json());
     app.use(router);
+
     return app;
 }
 
